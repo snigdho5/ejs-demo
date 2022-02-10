c19f3c49e3a7c3afd29d5c0fb629f6a5d7ae8227
Snigdho Upadhyay <snigdhoupadhyay@outlook.com>
2022-02-10 18:05:14 +0530

    changes

--- server.js
+++ server.js
@@ -6,7 +6,17 @@ const port = 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('pages/index');
+    var mascots = [
+        { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012 },
+        { name: 'Tux', organization: "Linux", birth_year: 1996 },
+        { name: 'Moby Dock', organization: "Docker", birth_year: 2013 }
+    ];
+    var tagline = "No programming concept is complete without a cute animal mascot.";
+
+    res.render('pages/index', {
+        mascots: mascots,
+        tagline: tagline
+    });
 });
 
 // app.get('/', function (req, res) {
