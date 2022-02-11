566e7a8bd7633337585fcdbd0e9fa07679447477
Snigdho Upadhyay <snigdhoupadhyay@outlook.com>
2022-02-11 17:06:05 +0530

    router

--- server.js
+++ server.js
@@ -5,27 +5,9 @@ const port = 3000;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    var mascots = [
-        { name: 'John', organization: "Java", birth_year: 2016 },
-        { name: 'Smith', organization: "Linux", birth_year: 1995 },
-        { name: 'Coby', organization: "Docker", birth_year: 2014 }
-    ];
-    var tagline = "Hello World!";
+var routes = require('./routes.js');
+app.use('/', routes);
 
-    res.render('pages/index', {
-        mascots: mascots,
-        tagline: tagline
-    });
-});
-
-// app.get('/', function (req, res) {
-//     res.render('pages/index');
-// });
-
-app.get('/about', function (req, res) {
-    res.render('pages/about');
-});
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
