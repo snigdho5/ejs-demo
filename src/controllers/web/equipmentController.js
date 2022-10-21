4c091f4fb4b020e1ee8b8f634e8cb72bf8a37ab2
Madhumoy Ghosh <59534839+madhumoyghosh@users.noreply.github.com>
2022-10-21 12:25:19 +0530

    Equipments

---
+++ src/controllers/web/equipmentController.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var router = express.Router();
+var moment = require("moment");
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const mime = require("mime");
+const Users = require("../../models/web/usersModel");
+// const helper = require("../helpers/helper");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const tokenSecret = "a2sd#Fs43d4G3524Kh";
+const rounds = 10;
+const dateTime = moment().format("YYYY-MM-DD h:mm:ss");
+const auth = require("../../middlewares/auth");
+// var { getAllActiveSessions } = require("../../middlewares/redis");
+const { check, validationResult } = require("express-validator");
+// var uuid = require("uuid");
+var crypto = require("crypto");
+var randId = crypto.randomBytes(20).toString("hex");
+const multer = require("multer");
+
+//methods
+
+exports.getData = async function (req, res, next) {
+  // Validate request parameters, queries using express-validator
+
+  var pageName = "Equipment";
+  var pageTitle = req.app.locals.siteName + " - " + pageName + " List";
+
+  res.render("pages/equipment/list", {
+    siteName: req.app.locals.siteName,
+    pageName: pageName,
+    pageTitle: pageTitle,
+    userFullName: req.session.user.name,
+    userImage: req.session.user.image_url,
+    userEmail: req.session.user.email,
+    year: moment().format("YYYY"),
+    requrl: req.app.locals.requrl,
+    status: 0,
+    message: "found!",
+    respdata: {},
+  });
+};
+
+exports.addData = async function (req, res, next) {
+    var pageName = "Equipment";
+    var pageTitle = req.app.locals.siteName + " - Add " + pageName;
+  res.render("pages/equipment/create", {
+    siteName: req.app.locals.siteName,
+    pageName: pageName,
+    pageTitle: pageTitle,
+    userFullName: req.session.user.name,
+    userImage: req.session.user.image_url,
+    userEmail: req.session.user.email,
+    year: moment().format("YYYY"),
+    requrl: req.app.locals.requrl,
+    status: 0,
+    message: "found!",
+    respdata: {},
+  });
+};
